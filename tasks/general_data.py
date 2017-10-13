e0881c10308f913f6c391dab8bbd06a119e68dc2
guillochon <guillochon@gmail.com>
2017-10-13 16:55:06 -0400

    MAINT: still trying to fix merging

--- tasks/general_data.py
+++ tasks/general_data.py
@@ -232,16 +232,16 @@ def do_internal(catalog):
 
         for alias in new_entry.get_aliases():
             if catalog.entry_exists(alias):
-                name = catalog.get_preferred_name(alias)
-                if catalog.entries[name]._stub:
-                    old_entry = Kilonova.init_from_file(catalog, name=name)
+                old_name = catalog.get_preferred_name(alias)
+                if catalog.entries[old_name]._stub:
+                    old_entry = Kilonova.init_from_file(catalog, name=old_name)
                 else:
-                    old_entry = catalog.entries[name]
+                    old_entry = catalog.entries[old_name]
                 break
 
         if old_entry:
             catalog.copy_entry_to_entry(new_entry, old_entry)
-            catalog.entries[name] = old_entry
+            catalog.entries[old_name] = old_entry
         else:
             catalog.entries[name] = new_entry
 
